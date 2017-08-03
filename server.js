f44019cd9e9c8160fd55d6f93b10d6401fbb69b7
Joe Churchman <churchman.joe@gmail.com>
2017-08-03 13:50:55 -0700

    project boilerplate and server.js established

---
+++ server.js
@@ -0,0 +1,14 @@
+const app = require('./lib/app');
+const http = require('http');
+
+const connect = require('./lib/connect');
+const dbUri = process.env.MONGODB_URI || 'mongodb://localhost:27017/backend-quiz';
+connect(dbUri);
+
+const server = http.createServer(app);
+
+const port = process.env.PORT || 3000;
+
+server.listen(port, () => {
+    console.log('server up and running on port: ', server.address()); //eslint-disable-line
+});
