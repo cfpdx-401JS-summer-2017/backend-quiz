7b01534df7a2b41ffa733a32aa615ae7bd62ccd6
Meryl Turner <merylturner08@gmail.com>
2017-08-03 14:25:47 -0700

    get restaurant test passing

--- tests/e2e/restaurants.test.js
+++ tests/e2e/restaurants.test.js
@@ -44,8 +44,9 @@ describe('restaurant API', () => {
             .send(restaurant)
             .then(({ body }) => {
                 restaurant._id = body._id;
-                // restaurant.__v = body.__v;
+                restaurant.__v = body.__v;
                 restaurant.reviews = body.reviews;
+                restaurant.reviews._id = body.reviews._id;
                 return body;
             });
     }
@@ -55,7 +56,23 @@ describe('restaurant API', () => {
             .then(savedRestaurant => {
                 assert.ok(savedRestaurant._id);
                 assert.equal(savedRestaurant.name, deschutes.name);
+                assert.equal(savedRestaurant.reviews, deschutes.reviews);
             });
+    });
+
+    it('gets a restaurant if it exists', () => {
+        return request
+            .get(`/restaurants/${deschutes._id}`)
+            .then(res => res.body)
+            .then(restaurant => {
+                console.log('restaurant is', restaurant);
+                console.log('deschutes is', deschutes);
+                assert.deepEqual(restaurant, deschutes);
+            });
+    });
+
+    xit('gets all the restaurants', () => {
+
     });
 
 });
